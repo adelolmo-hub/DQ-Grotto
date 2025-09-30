ecb5973c8e9b10dbf3e07b55324ad36fb3216aad
Albert del Olmo <albert.delolmo@gmail.com>
2025-09-30 15:15:44 +0200

    First Commit

---
+++ src/main/java/app/dqproject/DqGrottoApplication.java
@@ -0,0 +1,13 @@
+package app.dqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DqGrottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DqGrottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/dqproject/DqGrottoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.dqproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DqGrottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
