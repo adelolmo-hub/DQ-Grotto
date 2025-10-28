6626ce582332fcbe973840021abdd0c60a5f3229
Albert del Olmo <albert.delolmo@gmail.com>
2025-10-28 17:00:40 +0100

    Login+Register Added

--- src/main/java/app/dqproject/service/AuthService.java
+++ src/main/java/app/dqproject/service/AuthService.java
@@ -1,12 +1,13 @@
 package app.dqproject.service;
 
 import java.util.Date;
-import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import app.dqproject.dto.UserLoginDTO;
 import app.dqproject.exceptions.EntityNotFoundException;
 import app.dqproject.models.User;
 import app.dqproject.repository.IUserRepository;
@@ -20,17 +21,26 @@ public class AuthService {
 	@Autowired private IUserRepository userRepository;
 	@Autowired private PasswordEncoder passwordEncoder;
 	
-	private final String secretKey = "test";
+	@Value("${jwt.secret}")
+	private String secretKey;
 	
-	public String login(String email, String password) {
-		User user = userRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException(1));
+	public String login(UserLoginDTO userDTO) {
+		User user = userRepository.findByEmail(userDTO.getEmail()).orElseThrow(() -> new EntityNotFoundException(1));
 		
-		if(!passwordEncoder.matches(password, user.getPassword())) {
+		if(!passwordEncoder.matches(userDTO.getPassword(), user.getPassword())) {
 			throw new RuntimeException("Contraseña incorrecta");
 		}
 		return Jwts.builder().setSubject(user.getId())
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
 				.signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256).compact();
+	}
+	
+	public User createUser(User user) {
+		if(userRepository.existsByEmail(user.getEmail())) {
+			throw new IllegalStateException("User with email " + user.getEmail() + " already exists");
+		}
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
 	}
 }
--- src/main/java/app/dqproject/service/SecurityConfig.java
+++ src/main/java/app/dqproject/service/SecurityConfig.java
@@ -3,8 +3,10 @@ package app.dqproject.service;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,10 +21,10 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/api/v1/auth/**").permitAll()
+				.requestMatchers(HttpMethod.POST ,"/api/v1/auth/**").permitAll()
 				.anyRequest().authenticated()
 			).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+		http.csrf(AbstractHttpConfigurer::disable);
 		return http.build();
 	}
 	
